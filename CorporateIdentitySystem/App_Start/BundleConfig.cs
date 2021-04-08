2c2cc707e55f02da2b8e4c358b7744c9a96fac3e
annylue330 <annyblue330@gmail.com>
2021-04-08 09:48:12 +0800

    init

---
+++ CorporateIdentitySystem/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace CorporateIdentitySystem
+{
+    public class BundleConfig
+    {
+        // 如需統合的詳細資訊，請瀏覽 https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.bundle.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                    "~/Content/vendor/bootstrap.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                    "~/Content/layout.min.css"));
+        }
+    }
+}
---
+++ CorporateIdentitySystem/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CorporateIdentitySystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CI_Layout()
+        {
+            return View();
+        }
+        public ActionResult Header_Demo1()
+        {
+            return View();
+        }
+        public ActionResult Header_Demo2()
+        {
+            return View();
+        }
+        public ActionResult Header_Demo3()
+        {
+            return View();
+        }
+        public ActionResult Footer_Demo1()
+        {
+            return View();
+        }
+
+        public ActionResult CI_Tab()
+        {
+            return View();
+        }
+
+        public ActionResult Tab1_Demo()
+        {
+            return View();
+        }
+        public ActionResult Tab2_Demo()
+        {
+            return View();
+        }
+        public ActionResult CI_Icon()
+        {
+            return View();
+        }
+        public ActionResult CI_Button()
+        {
+            return View();
+        }
+        public ActionResult CI_Message()
+        {
+            return View();
+        }
+        public ActionResult CI_Loader()
+        {
+            return View();
+        }
+        public ActionResult CI_Forms()
+        {
+            return View();
+        }
+        public ActionResult CI_Table()
+        {
+            return View();
+        }
+        public ActionResult CI_Popups()
+        {
+            return View();
+        }
+
+        public ActionResult Message_Demo()
+        {
+            return View();
+        }
+    }
+}
